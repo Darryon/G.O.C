43dbb613e43469a9877c472eca53bf21be667950
djk239 <dale.king@rutgers.edu>
2024-01-18 13:56:29 -0500

    player class

--- Player.py
+++ Player.py
@@ -1,7 +1,22 @@
 class Player:
-    def __init__(self, health, damage, myclass):
-        self.health = health
-        self.damage = damage
-        self.myclass = myclass
+    def __init__(self, name):
+        self.__name = name
+        self.__health = 10
+        self.__damage = 2
+        self.__class = "NULL"
 
+    def getHealth(self):
+        return self.__health
+
+    def setHealth(self, newhealth):
+        self.__health = newhealth
+
+    def getname(self):
+        return self.__name
+
+    def setname(self, newname):
+        self.__name = newname
+
+    def takeDamage(self, damage):
+        self.__health = self.__health - damage
 
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from Map import Map
 from Player import Player
 
+#----------------------------------------------------------------
+# Game Runner below
+#----------------------------------------------------------------
+Forest = Map()
+def Start():
+    temp = input("Hello travler, Please state your name.\n")
+    character = Player(name=temp)
+    print(f"Hello {character.getname()}, thats a nice name.")
+    print(f"Well anyways {character.getname()}, there's no time to waste. Take this")
+    input()
+    print("*You drink a suspicious drink*\n")
 
-def main():
-    mymap = Map()
-    
-    for i in range(0, 29):
-        for k in range(0,29):
-            print(mymap.grid[i][k], end="")
-        print("\n")
+    print("!You awake in a clearing. To the west is an abandoned house. All around you is forest.")
+    print("!What shall you do first?")
+
+
+def Walk(direction):
+    Forest.move(direction)
+
+def LookAround():
+    #will return the left right top down objects in map based on player position
+    print()
+
 
 
 if __name__ == "__main__":
-    main()
+    Start()
